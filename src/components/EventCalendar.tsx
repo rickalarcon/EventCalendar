e46f63bb8f2089205d2bc83f9a09cc7110f1e2fc
rick alarcon <jefry_alarcon@hotmail.com>
2020-11-23 09:19:25 -0800

    updated styling and calendar indicator/generator

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -12,3 +12,131 @@ interface DayBoxProps {
   readonly outOfRange?: boolean;
   readonly isSelected?: boolean;
 }
+
+
+
+
+const EventCalendar = ({ onDaySelected, eventDates, onMonthSelected }: Interprops) => {
+  const [selectedDay, setSelectedDay] = useState<moment.Moment>(moment());
+  const [selectedMonth, setSelectedMonth] = useState<moment.Moment>(moment());
+  const [events, setEvents] = useState<number[]>([]);
+
+  const previousMonthDays = () => {
+    let previousDays: JSX.Element[] = [];
+    let firstOfMonth = selectedMonth.startOf("month");
+
+    // Check if Month starts with a Sunday
+    if (firstOfMonth.day() !== 0) {
+      let previousSunday = moment(selectedMonth)
+        .subtract(1, "months")
+        .endOf("month")
+        .day("Sunday");
+      let dayDifference = firstOfMonth.diff(previousSunday, "day") + 1;
+
+      for (let i = 0; i < dayDifference; i++) {
+        let date = moment(firstOfMonth);
+        date.subtract(dayDifference - i, "day");
+        previousDays.push(
+          <DayBox
+            isSelected={date.isSame(selectedDay, "day")}
+            onClick={() => setSelectedDay(date)}
+            outOfRange={true}
+            key={i + 31}
+          >
+            <Day>{date.format("D")}</Day>
+          </DayBox>
+        );
+      }
+    }
+    return previousDays;
+  };
+
+  const getDaysOfMonth = () => {
+    let daysOfMonth: JSX.Element[] = [];
+
+    for (let i = 1; i <= selectedMonth.daysInMonth(); i++) {
+      let date = moment(selectedMonth);
+      date.set("date", i);
+      daysOfMonth.push(
+        <DayBox
+          isSelected={date.isSame(selectedDay, "day")}
+          onClick={() => setSelectedDay(date)}
+          key={i}
+        >
+          <Day>{i}</Day>
+          <EventMarkerWrapper>
+            {events.length > 0 &&
+              [...new Array(events[i - 1])].map((x, index) => (
+                <EventMarker key={index} />
+              ))}
+          </EventMarkerWrapper>
+        </DayBox>
+      );
+    }
+
+    return daysOfMonth;
+  };
+
+  const Rowsgetter = () => {
+    let slots: JSX.Element[] = [...previousMonthDays(), ...getDaysOfMonth()];
+    let cells: JSX.Element[] = [];
+
+    return slots.reduce(
+      (prev: JSX.Element[][], curr, index) => {
+        if (index % 7 === 0) {          
+          prev.push(cells);
+          cells = [];
+        }
+        // Push current cell to cells
+        cells.push(curr);
+
+        //push last row when reached the end
+        if (index === slots.length - 1) {
+          prev.push(cells);
+        }
+        return prev;
+      },
+      [[]]
+    );
+  };
+
+  const onChangeMonth = (months: number) => {
+    let newSelectedMonth = moment(selectedMonth);
+    newSelectedMonth.add(months, "months");
+    setSelectedMonth(newSelectedMonth);
+  };
+
+  useEffect(() => {
+    if (!selectedDay.isSame(selectedMonth, "month")) {
+      let newMonth = moment(selectedMonth);
+      newMonth.set("month", selectedDay.get("month"));
+      setSelectedMonth(newMonth);
+    }
+
+    onDaySelected(selectedDay);
+  }, [selectedDay ]);
+
+  useEffect(() => {
+    let eventArray = [...Array(31)].map(x => 0);
+    eventDates.forEach(x => {
+
+        if(moment(x).isSame(selectedMonth, 'month')) {
+            let index = +x.format('D') - 1;
+            eventArray[index] = eventArray[index] === 3 ? 3 : eventArray[index] + 1;
+        }
+    });
+    setEvents(eventArray);
+  }, [selectedMonth])
+
+  useEffect(() => {
+    onMonthSelected(selectedMonth);
+  }, [selectedMonth])
+
+  return (
+    <Wrapper>
+
+    </Wrapper>
+  );
+};
+
+  export default EventCalendar;
--- src/components/calendar-style.js
+++ src/components/calendar-style.js
@@ -21,3 +21,66 @@ export const GetMonthButton = styled.button`
     outline: none;
   }
 `;
+
+
+export const Month = styled.p`
+  font-weight: 700;
+  font-size: 14px;
+  margin: 0 0 0 8px;
+  color: rgb(0, 102, 204);
+
+`;
+
+export const ChoiceMonth = styled.div`
+  display: flex;
+  margin: 10px 0;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Year = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  padding-left: 5px;
+`;
+
+
+export const WeekDay = styled.th`
+  color: #92929d;
+  text-transform: uppercase;
+  font-size: 12px;
+  width: 48px;
+  margin: 0 5px;
+  height: 39px;
+`;
+
+export const SelectedDayStyles = css`
+  background-color: rgb(35, 183, 246); //light blue ish
+  color: white;                        //text color for the selected day!
+  border-radius: 4px;
+`;
+
+export const EventMarkerWrapper = styled.div`
+    width: 100%;
+  position: absolute;
+  bottom: 10px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const EventMarker = styled.div`
+  height: 5px;
+  width: 5px;
+  margin: 0px 1px;
+  border-radius: 50%;
+  background-color: rgb(35, 183, 246);
+`;
+
+export const Day = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90%;
+  margin: 15%;
+`;
