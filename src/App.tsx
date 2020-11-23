141736082b0083cf1ce9a425bf4148d731a68409
rick alarcon <jefry_alarcon@hotmail.com>
2020-11-23 01:47:50 -0800

    generating numbers for days logic

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,28 @@ const moment = require("moment");
 
  
 
+ const generateRandomDays = (currentMonth: moment.Moment) => {
+    let events = [];
+    let maxDay = +currentMonth.endOf("month").format("D");
+    for (let i = 0; i < 40; i++) {
+      let newDate = moment(currentMonth);
+      newDate.set(
+        "date", 
+        1 + Math.floor((maxDay - 1) * Math.random()) //randomDay
+        );
+      events.concat(newDate);
+    }
+    return events;
+  };
+
 function App() {
   return (
     <div className="App">
      "HELLO!"
+    
+
+
+
     </div>
   );
 }
