1dd8fb35c0648a52efb9ff9579135ea8336e7aaa
rick alarcon <jefry_alarcon@hotmail.com>
2020-11-23 01:27:20 -0800

    included basic styling and interfaces

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,3 +1,14 @@
 import React, { useState, useEffect } from "react";
 import moment from "moment";
 import styled from "styled-components";
+
+
+interface Interprops {
+  onDaySelected: (selectedDay: moment.Moment) => void;
+  eventDates: moment.Moment[];
+  onMonthSelected: (selectedMonth: moment.Moment) => void;
+}
+interface DayBoxProps {
+  readonly outOfRange?: boolean;
+  readonly isSelected?: boolean;
+}
--- src/components/calendar-style.js
+++ src/components/calendar-style.js
@@ -3,3 +3,21 @@ import styled, { css } from "styled-components";
 export const Wrapper = styled.div`
   max-width: 400px;
 `;
+
+export const MonthButtons = styled.div``;
+
+export const GetMonthButton = styled.button`
+  margin: 0 5px;
+  border-radius: 6px;
+  border: 1px solid #e2e2ea;
+  padding: 0;
+  color: #92929d;
+  background-color: rgb(35, 246, 246);
+  padding: 6px;
+  &:hover {
+    cursor: pointer;
+  }
+  &:focus {
+    outline: none;
+  }
+`;
